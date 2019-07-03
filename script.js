36679f94aa98e13bf71ced79d74106979e2297e7
adrian-vasquez-dev <advas14@gmail.com>
2019-07-03 09:13:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/*Responsive Nav*/
+function myFunction() {
+  
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+
+    x.className = "topnav";
+  }
+}
+
+function mySearch() {
+	var query = document.getElementById('search-query').value;
+	window.open("http://prestonrichardson.agentsquared.com/search-results?Search="+query);
+}
+
+
+
+
+
+
+
+
